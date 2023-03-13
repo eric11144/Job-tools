c2dafe13531ee0cfd8c2f4700aee14808528c87a
peter <eric11144@yahoo.com.tw>
2023-03-13 14:24:25 +0800

    Modify the method of obtaining reboot count and the way it is recorded.

--- doraemon/script-test/test_reboot_count.sh
+++ doraemon/script-test/test_reboot_count.sh
@@ -6,12 +6,15 @@ filename="/home/ubuntu/log.txt"
 
 # Check if the reboot log file exists.
 if [ -f "$filename" ]; then
-    count=$(tail -n 1 $filename)
+    count=$(tail -n 1 $filename | awk '{print $1'}) # Get reboot count
     count=$(($count+1)) 
     echo $count 
-    echo $count >> $filename
-    echo $datetime >> $filename
+    echo $count ' ' $datetime >> $filename
     reboot
 else
+    # If the reboot log file does not exist, then create it by using the 'touch' command.
     touch "/home/ubuntu/log.txt"
+    echo $count 
+    echo $count ' ' $datetime >> $filename
+    reboot
 fi
