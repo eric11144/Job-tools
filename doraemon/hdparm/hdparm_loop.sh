b0f34701547815320e58732b42f48da519e40d51
Peter <eric11144@yahoo.com.tw>
2023-05-12 17:16:55 +0800

    Update hdparm test loop

--- doraemon/hdparm/hdparm_loop.sh
+++ doraemon/hdparm/hdparm_loop.sh
@@ -2,7 +2,7 @@
 
 echo "Input runtime number: $1"
 
-echo "hdparm test loop"
+echo "Start hdparm test"
 
 speed=0
 total=0
@@ -10,10 +10,13 @@ total=0
 for((i=1; i<= $1; ++i))
 do 
     echo "Runtime: $i"
-    speed=$(hdparm -t --direct /dev/sda1 | grep "seconds" | cut -d '=' -f2 | cut -d ' ' -f2)
-    echo $speed
+    speed=$(hdparm -t --direct /dev/nvme0n1 | grep "seconds" | cut -d '=' -f2 | cut -d ' ' -f2)
+    speed_unit=$(hdparm -t --direct /dev/nvme0n1 | grep "seconds" | cut -d '=' -f2 | cut -d ' ' -f3)
+    echo "Current: $speed $speed_unit"
     total=$(awk 'BEGIN{printf "%.2f\n", ('$total'+'$speed')}')
 done
 
-echo "scale=2; $total / $1" | bc
+average=$(echo "scale=2; $total / $1" | bc)
+
+echo "Average: $average $speed_unit"
 echo "hdparm test finish"
